107766e53e4daa8a976f46116ec4c905d958c993
park <67350808+wjwjsk@users.noreply.github.com>
2023-08-22 15:13:10 +0900

    Update settings.py

--- boardtest/settings.py
+++ boardtest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o_0!)q9zp)#$9q_2z)vzqaa4t#kn)7r!x+&bsl8z6)y=f&1#-i"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
